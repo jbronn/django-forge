990a4772b49308105e7b1ea347b09a4e4a790649
Justin Bronn <jbronn@gmail.com>
2013-03-01 11:05:40 -0800

    Added admin inlines for modules and releases.

--- forge/admin.py
+++ forge/admin.py
@@ -3,15 +3,6 @@ from django.contrib import admin
 from .models import Author, Module, Release
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
-
-
-class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('author', 'name')
-    search_fields = ('name', 'author__name', 'tags', 'desc')
-
-
 class ReleaseAdmin(admin.ModelAdmin):
     list_display = ('module', 'version')
     search_fields = ('version',
@@ -19,6 +10,33 @@ class ReleaseAdmin(admin.ModelAdmin):
                      'module__author__name',
                      'module__tags',
                      'module__desc')
+
+
+class ReleaseInline(admin.TabularInline):
+    model = Release
+    extra = 1
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name')
+    search_fields = ('name', 'author__name', 'tags', 'desc')
+
+    inlines = [
+        ReleaseInline,
+    ]
+
+
+class ModuleInline(admin.TabularInline):
+    model = Module
+    extra = 1
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+    inlines = [
+        ModuleInline,
+    ]
 
 
 admin.site.register(Author, AuthorAdmin)
