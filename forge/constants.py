4141240a933613c7ea56e992876211c5bf546391
Justin Bronn <justin@counsyl.com>
2014-05-31 13:40:58 -0700

    Modified variable names for the Puppet Labs Forge API urls.

--- forge/constants.py
+++ forge/constants.py
@@ -7,7 +7,8 @@ FORGE_HOME = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
 MODULE_REGEX = re.compile(r'^(?P<author>\w+)[/\-](?P<module>\w+)$')
 TARBALL_REGEX = re.compile(r'^(?P<author>\w+)-(?P<module>\w+)-(?P<version>[\w\.]+)\.tar\.gz$')
 
-PUPPET_FORGE = 'https://forge.puppetlabs.com'
+PUPPETLABS_FORGE_URL = 'https://forge.puppetlabs.com'
+PUPPETLABS_FORGE_API_URL = 'https://forgeapi.puppetlabs.com'
 MODULES_JSON = '/modules.json'
 RELEASES_JSON = '/api/v1/releases.json'
 RELEASES_URL = '/system/releases/'
