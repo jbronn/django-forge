aaca68866a35ae1b99c3d111535d3e09fa97394a
Justin Bronn <justin@counsyl.com>
2014-06-01 13:40:37 -0700

    Added logging, improved error handling, and added a `query` keyword argument.

--- forge/client.py
+++ forge/client.py
@@ -1,11 +1,16 @@
 import json
-import requests
+import logging
 import time
 import urllib
 import urlparse
 
+import requests
+
 from . import __version__
 from . import constants
+
+
+logger = logging.getLogger('forge.client')
 
 
 class ForgeClient(object):
@@ -13,65 +18,70 @@ class ForgeClient(object):
     def __init__(self, api_url=constants.PUPPETLABS_FORGE_API_URL,
                  api_version=3, agent_type=None):
         """
-        TODO
+        This is an object that acts as client for the Forge according
+        to the given parameters.
         """
-        # Use of Puppet Labs' forge requires a user agent is set:
+        # Use of Puppet Labs' forge requires a user agent:
         #  https://forgeapi.puppetlabs.com/#user-agent-required
         user_agent = 'django-forge/%s' % __version__
         if agent_type:
             user_agent += ' (%s)' % agent_type
+        logger.info('setting client user agent to %s' % user_agent)
         self.user_agent = user_agent
-        self.api_url = '%s/v%d/' % (api_url, api_version)
+        self.api_url = urlparse.urljoin(api_url, 'v%d/' % api_version)
 
-    def get(self, url, headers=None):
-        request_headers = {
-            'User-Agent': self.user_agent,
-        }
-        if headers:
-            request_headers.update(headers)
-
-        return requests.get(url, headers=request_headers)
+    def get(self, url, **headers):
+        headers.setdefault('User-Agent', self.user_agent)
+        return requests.get(url, headers=headers)
 
 
 class ForgeAPI(object):
-    def __init__(self, client, endpoint, limit=20, throttle=0):
+    def __init__(self, client, endpoint, limit=20, query=None, throttle=0):
         # Setting instance variables.
         self.client = client
         self.endpoint = endpoint
         self.limit = limit
         self.throttle = throttle
+        self.query = query or {}
 
         # Joining the client's API URL with that of the given endpoint.
         self.api_url = urlparse.urljoin(self.client.api_url, self.endpoint)
 
         # Performing the initial API request to get the total object count and
         # prime the initial results.
-        initial_data = self.api_request(self.url())
+        initial_data = self.request(self.url(**self.query))
         self.total = initial_data['pagination']['total']
         self.results = initial_data['results']
 
     def __iter__(self):
+        query = self.query.copy()
         for idx in xrange(len(self)):
             if idx >= len(self.results):
                 if self.throttle:
                     time.sleep(self.throttle)
-                data = self.api_request(self.url(offset=idx))
+                query['offset'] = idx
+                data = self.request(self.url(**query))
                 self.results.extend(data['results'])
             yield self.results[idx]
 
     def __len__(self):
         return self.total
 
-    def api_request(self, url):
+    def request(self, url):
+        logger.info('making API request: %s' % url)
         req = self.client.get(url)
-        if not req.status_code == 200:
-            raise Exception('Could not perform Forge API request.')
+        try:
+            req.raise_for_status()
+        except Exception as e:
+            logger.exception(e)
+            raise
         return json.loads(req.content)
 
-    def url(self, offset=0):
+    def url(self, **query):
+        # Currently, the forge doesn't allow any modification to the limit.
+        query['limit'] = self.limit
         splits = urlparse.urlsplit(self.api_url)
-        query = urllib.urlencode({'limit': self.limit, 'offset': offset})
         return urlparse.urlunsplit(
             (splits.scheme, splits.netloc, splits.path,
-             query, splits.fragment)
+             urllib.urlencode(query), splits.fragment)
         )
