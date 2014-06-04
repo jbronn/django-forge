ff6272f8772895146abff51ea41a6b68f72e36ff
Justin Bronn <jbronn@gmail.com>
2014-06-03 17:15:52 -0700

    Got `/v3/releases` into state that's good enough for Puppet 3.6 to handle.

--- forge/views/v3.py
+++ forge/views/v3.py
@@ -21,8 +21,57 @@ def error_response(errors, **kwargs):
     )
 
 
-def paginated_response():
-    pass
+def pagination_data(qs, query, url_name):
+    """
+    Returns a two-tuple comprising a Page and dictionary of pagination data
+    corresponding to the given queryset, query dictionary, and URL name.
+    """
+    limit = query.get('limit', 20)
+    offset = query.get('offset', 0)
+
+    p = Paginator(qs, limit)
+    page_num = (offset / p.per_page) + 1
+    page = p.page(page_num)
+
+    cur_url = urlparse.urlsplit(reverse(url_name))
+    first_query = query.copy()
+    first_query['offset'] = 0
+    first_url = urlparse.urlunsplit(
+        (cur_url.scheme, cur_url.netloc, cur_url.path,
+         urllib.urlencode(first_query), cur_url.fragment)
+    )
+
+    if page.has_previous():
+        prev_query = query.copy()
+        prev_query['offset'] = (page_num - 2) * p.per_page
+        prev_url = urlparse.urlunsplit(
+            (cur_url.scheme, cur_url.netloc, cur_url.path,
+             urllib.urlencode(prev_query), cur_url.fragment)
+        )
+    else:
+        prev_url = None
+
+    if page.has_next():
+        next_query = query.copy()
+        next_query['offset'] = page_num * p.per_page
+        next_url = urlparse.urlunsplit(
+            (cur_url.scheme, cur_url.netloc, cur_url.path,
+             urllib.urlencode(next_query), cur_url.fragment)
+        )
+    else:
+        next_url = None
+
+    pagination_dict = {
+        'limit': limit,
+        'offset': offset,
+        'first': first_url,
+        'previous': prev_url,
+        'next': next_url,
+        'total': p.count,
+    }
+
+    return page, pagination_dict
+
 
 def modules(request):
     pass
@@ -44,7 +93,6 @@ def releases(request):
     except ValueError:
         offset = 0
 
-
     query = {
         'limit': limit,
         'offset': offset,
@@ -60,49 +108,28 @@ def releases(request):
         except Module.DoesNotExist:
             qs = qs.none()
 
-    p = Paginator(qs, limit)
-    page_num = (offset / p.per_page) + 1
-    page = p.page(page_num)
+    # Get pagination page and data.
+    page, pagination_dict = pagination_data(qs, query, 'releases_v3')
 
-    cur_url = urlparse.urlsplit(reverse('releases_v3'))
-    first_query = query.copy()
-    first_query['offset'] = 0
-    first_url = urlparse.urlunsplit(
-        (cur_url.scheme, cur_url.netloc, cur_url.path,
-         urllib.urlencode(first_query), cur_url.fragment)
-    )
-
-    if page.has_previous():
-        prev_query = query.copy()
-        prev_query['offset'] = page_num * (p.per_page - 1)
-        prev_url = urlparse.urlunsplit(
-            (cur_url.scheme, cur_url.netloc, cur_url.path,
-             urllib.urlencode(prev_query), cur_url.fragment)
-        )
-    else:
-        prev_url = None
-
-    if page.has_next():
-        next_query = query.copy()
-        next_query['offset'] = page_num * p.per_page
-        next_url = urlparse.urlunsplit(
-            (cur_url.scheme, cur_url.netloc, cur_url.path,
-             urllib.urlencode(next_query), cur_url.fragment)
-        )
-    else:
-        next_url = None
+    releases = []
+    for release in page.object_list:
+        releases.append({
+            'module': {
+                'name': release.module.name.lower(),
+                'owner': {
+                    'name': release.module.author.name.lower(),
+                }
+            },
+            'metadata': release.metadata,
+            'version': str(release.version),
+            'file_uri': release.tarball.url,
+            'file_size': release.file_size,
+            'file_md5': release.file_md5,
+        })
 
     releases_data = {
-        'pagination': {
-            'limit': limit,
-            'offset': offset,
-            'first': first_url,
-            'previous': prev_url,
-            'next': next_url,
-            'total': p.count,
-        },
-        'results': [
-        ]
+        'pagination': pagination_dict,
+        'results': releases,
     }
 
     return json_response(releases_data, indent=2)
